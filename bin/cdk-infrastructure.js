56c7d4f84324ade459ec85700cafe53361c7f8af
David Poindexter <drpoindexter@gmail.com>
2022-10-13 13:35:48 -0400

    Add stack environment configuration

--- bin/cdk-infrastructure.js
+++ bin/cdk-infrastructure.js
@@ -4,18 +4,16 @@ const cdk = require("aws-cdk-lib");
 const { CDKGatsby } = require("../lib/cdk-infrastructure-stack");
 
 const app = new cdk.App();
+
 new CDKGatsby(app, "CDKGatsby", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
   env: { account: "310141637485", region: "us-east-2" },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  domain: "cleanslatetg.cloud",
+  subdomain: "garden",
+  tags: {
+    environment: "prod",
+    duration: "temporary",
+    cdk: "true",
+    purpose: "presentation",
+    author: "david.poindexter@cleanslatetg.com",
+  },
 });
